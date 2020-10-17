b13d60da39760915b9fa80dd56d750979ba7e8eb
JoannaChang <joannahanchang@gmail.com>
2020-10-16 23:30:17 -0700

    Assignment #1 Task #2

--- node-express/routes/campsiteRouter.js
+++ node-express/routes/campsiteRouter.js
@@ -11,11 +11,11 @@ campsiteRouter.route('/')
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
-.get((req, res) => {
-    res.end('Will send all the campsites to you');
-})
 .post((req, res) => {
     res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
 })
 .put((req, res) => {
     res.statusCode = 403;
@@ -32,15 +32,14 @@ campsiteRouter.route('/:campsiteId')
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
 .get((req, res) => {
     res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
 })
-.post((req, res) => {
-    // res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-})
 .put((req, res) => {
-    res.statusCode = 403;
     res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
     res.end(`Will update the campsite: ${req.body.name}
         with description: ${req.body.description}`);
